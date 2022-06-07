28eb71565e3afb9ef427ed8ec4d0f4836314dc75
denizmck <dmalik@caseking.de>
2022-06-07 14:55:03 +0200

    Rebuild Structure for views

--- src/app/views/view.module.ts
+++ src/app/views/view.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { ViewRoutingModule } from './view-routing.module';
 
-import { HomePageRoutingModule } from './home-routing.module';
+// Views
+import { HomePage } from './home/home.page';
 
 
 @NgModule({
+  declarations: [HomePage],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+    ViewRoutingModule
+  ]
 })
-export class HomePageModule {}
+export class ViewModule { }
